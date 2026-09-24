94e9597e09360b62bc43f667836785782af971a3
13Louai <l.entamene@gmail.com>
2026-09-24 10:46:46 +0200

    finish day 2

--- Day2/day2exercice.py
+++ Day2/day2exercice.py
@@ -1,3 +1,5 @@
+import math 
+
 #Exercice 1 
 
 
@@ -19,4 +21,42 @@ type(first_name)
 type(last_name)
 type(full_name)
 type(country)
-type
+type(age)
+type(year)
+type(is_married)
+type(is_True)
+type(is_Light_on)
+type(name_of_pet)
+type(number_of_brother)
+type(city)
+
+len_fisrt_name = len(first_name)
+len_last_name = len(last_name)
+
+num_one = 5 
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one % num_two
+remainder = num_two / num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+
+radius = 30 
+area_of_circle = math.pi * radius ** 2
+circum_of_circle = 2 * math.pi * radius
+
+radius_user = input("Write ur radius")
+
+area_of_circle_user = math.pi * radius_user ** 2
+
+print(area_of_circle_user)
+
+last_name_user = input("What's ur last name ? ")
+first_name_user = input("What's ur first name ? ")
+age_user = input("What's ur age ? ")
+country_user = input("What's ur country ? ")
+
